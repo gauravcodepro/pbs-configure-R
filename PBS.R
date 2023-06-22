8ba62369de8da24385c3a40f5df6ba36a4a669d2
Gaurav Sablok <sablokg@gmail.com>
2023-06-22 15:14:25 +0530

    Create PBS.R

---
+++ PBS.R
@@ -0,0 +1,21 @@
+pbsCHCPC <- function(job, log, mail, host, queue, workdir, nodes, cpus, command, module){
+    print(paste0(PBS_JOBNAME = "job"))
+    print(paste0(PBS_O_LOGNAME = "job")
+    print(paste0(PBS_O_MAIL = "log"))
+    print(paste0(PBS_O_HOST = "host"))
+    print(paste0(PBS_O_QUEUE = "queue"))
+    print(paste0(PBS_O_WORKDIR = "workdir"))
+    print(paste0(PBS_NODES = "nodes"))
+    print(paste0(PBS_NCPUS = "cpu"))
+    print(paste0(PBS_COMMAND = "command"))
+    print(paste0(PBS_MODULE = "module"))
+}  
+pbsMake <- function() {
+pbs_check <- c("job", "log", "mail", "host", "queue", "workdir", "nodes", "cpus", "command", "module")
+pbs_vector <- as.vector(length(pbs_check))
+for (i in seq_along(pbs_check)){
+    pbs_vector[i] <- paste("PBS", "O", pbs_check[i], sep = "_")
+}
+for (i in seq_along(pbs_vector)){
+    print(paste(pbs_vector[i], "=", sep = " "))
+}}  
